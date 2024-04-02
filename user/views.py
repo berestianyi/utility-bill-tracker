ed3c771c2782766bb8365a2eba2301dd69a4505e
Ivan Berestianyi <berestvania@gmail.com>
2024-04-02 10:35:33 +0300

    wrote a user login view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,35 @@
+from django.contrib import auth
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from user.forms import UserLoginForm
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('products:home'))
+    else:
+        form = UserLoginForm()
+
+    context = {'form': form}
+    return render(request, 'user/login.html', context=context)
+
+
+def registration(request):
+    pass
+
+
+def profile(request):
+    pass
+
+
+def logout(request):
+    pass
