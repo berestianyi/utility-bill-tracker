3bfb3611508136aebc59405732ed27c3f7481e29
Ivan Berestianyi <berestvania@gmail.com>
2024-04-02 11:11:22 +0300

    added registration, profile change and logout views

--- user/views.py
+++ user/views.py
@@ -1,21 +1,21 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from user.forms import UserLoginForm
+from user.forms import UserLoginForm, UserRegistrationForm, UserProfileChangeForm
 
 
 def login(request):
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
-            username = request.POST['username']
+            email = request.POST['email']
             password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
+            user = auth.authenticate(email=email, password=password)
             if user and user.is_active:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('products:home'))
+                return HttpResponseRedirect(reverse('bills:index'))
     else:
         form = UserLoginForm()
 
@@ -24,12 +24,29 @@ def login(request):
 
 
 def registration(request):
-    pass
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'user/registration.html', context=context)
 
 
-def profile(request):
-    pass
+def profile_change(request):
+    if request.method == 'POST':
+        form = UserProfileChangeForm(data=request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+    else:
+        form = UserProfileChangeForm(instance=request.user)
+    context = {'form': form}
+    return render(request, 'user/profile.html', context=context)
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect('bills:index')
