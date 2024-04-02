dfd2441becdc3cf8a8fb6c984422e9d8f979a0ba
Ivan Berestianyi <berestvania@gmail.com>
2024-04-02 11:10:33 +0300

    added forms for registration and profile change

--- user/forms.py
+++ user/forms.py
@@ -5,9 +5,28 @@ from user.models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': "Enter your username"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': "Enter your email"}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': "Enter your password"}))
 
     class Meta:
         model = User
-        fields = ('username', 'password')
+        fields = ('email', 'password')
+
+
+class UserRegistrationForm(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': "Enter your username"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': "Enter your email"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': "Create password"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': "Repeat password"}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class UserProfileChangeForm(UserChangeForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'readonly': True}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
