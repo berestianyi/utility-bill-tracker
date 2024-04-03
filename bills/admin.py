cd4502e70e7b0e4c20cacd64febb73d8f26b3ae9
draakul <berestvania@gmail.com>
2024-04-03 23:24:52 +0300

    added new model to admin

--- bills/admin.py
+++ bills/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from bills.models import Bill, BillCategory, Building
+from bills.models import Bill, BillSubCategory, BillCategory, Building
 
 admin.site.register(Building)
 admin.site.register(BillCategory)
 admin.site.register(Bill)
+admin.site.register(BillSubCategory)
