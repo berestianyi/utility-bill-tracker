328385bec5fcd40dba6f33c8351d5e4a279b346b
draakul <berestvania@gmail.com>
2024-04-03 23:27:59 +0300

    created view for htmx

--- bills/views.py
+++ bills/views.py
@@ -1,11 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request,'bills/index.html')
+    return render(request, 'bills/index.html')
+
+
+def add_building(request):
+    return render(request, 'bills/add_building.html')
+
 
 def add_bill(request):
     pass
 
+
 def building(request):
     pass
+
+
+def add_input(request):
+    if request.method == 'POST':
+        # return HttpResponse('/bills/include/htmx_add_bill_input.html')
+        return render(request, 'bills/include/htmx_add_bill_input.html')
+    return HttpResponse("Invalid method", status=405)
