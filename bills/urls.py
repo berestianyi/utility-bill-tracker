94e28e48b03644b3afe803f2a88ac408e286bae5
draakul <berestvania@gmail.com>
2024-04-06 20:16:38 +0300

    change ulr view func name

--- bills/urls.py
+++ bills/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('add/', views.add_bill, name='add_bill'),
     path('add_building/', views.add_building, name='add_building'),
-    path('building/<int:building_key>/', views.building, name='building'),
+    path('building/<int:building_key>/', views.building_page, name='building'),
     path('add-input/', views.add_input, name='add-input'),
 ]
