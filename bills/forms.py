ff33431fbc58f6845475e59641bd1571b930c920
draakul <berestvania@gmail.com>
2024-04-09 21:54:40 +0300

    add billsform

--- bills/forms.py
+++ bills/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
-from bills.models import Building, BillSubCategory
+from bills.models import Building, BillSubCategory, Bill, BillCategory
+
+
+class CustomBillForm(forms.CharField):
+    pass
 
 
 class BuildingForm(forms.ModelForm):
@@ -34,3 +38,32 @@ class BuildingForm(forms.ModelForm):
     class Meta(object):
         model = Building
         fields = ('name', 'address', 'home_mark')
+
+
+class BillsForm(forms.ModelForm):
+    category = forms.ModelChoiceField(queryset=BillCategory.objects.all(),
+                                      widget=forms.Select(attrs={
+                                          "hx-get": "/load_bill_category/",
+                                          "hx-target": "#id_name",
+                                          "class": 'form-select',
+                                          "id": "inputGroupSelect02",
+                                          "style": "background-color: #1b1d1e; color: #ececec;"
+                                      }))
+    name = forms.ModelChoiceField(queryset=BillSubCategory.objects.none(),
+                                  widget=forms.Select(attrs={
+                                        "hx-get": "/load_measure_unit/",
+                                        "hx-target": "#measure_unit",
+                                        "class": 'form-select',
+                                        "id": "id_name",
+                                        "style": "background-color: #1b1d1e; color: #ececec;"}))
+
+    class Meta(object):
+        model = Bill
+        fields = ['name', 'amount', 'month_paid']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if "category" in self.data:
+            category_id = int(self.data["category"])
+            self.fields['name'].queryset = BillSubCategory.objects.filter(bill_category=category_id)
