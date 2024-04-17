c3050004bf08b5deef2f613900964793847f5332
Berestianyi Ivan <berestvania@gmail.com>
2024-04-17 19:21:39 +0300

    created new app called buildings and moved there all models, urls, views related to buildings

---
+++ buildings/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from user.models import User
+from bills.models import BillSubCategory
+
+
+class Building(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='buildings')
+    bill_sub_category = models.ManyToManyField(BillSubCategory, related_name='buildings', blank=True)
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    home_mark = models.BooleanField(default=False)
+    description = models.CharField(max_length=200, default='')
+
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.address
---
+++ buildings/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+
+from bills.forms import BillsForm
+from bills.models import Bill
+from buildings.forms import BuildingForm
+from buildings.models import Building
+
+
+def index(request):
+    buildings = Building.objects.all()
+    return render(request, 'buildings/index.html', {'buildings': buildings})
+
+
+def add_building(request):
+    if request.method == 'POST':
+        form = BuildingForm(request.POST)
+        if form.is_valid():
+            new_building = form.save(commit=False)
+            new_building.user = request.user
+            new_building.save()
+            form.save_m2m()
+            return redirect('bills:buildings')
+    else:
+        form = BuildingForm()
+    return render(request, 'buildings/add_building.html', {'form': form})
+
+
+def building_page(request, building_key):
+    building = Building.objects.get(pk=building_key)
+    bills = Bill.objects.all().filter(building=building_key)
+    if request.method == 'POST':
+        form = BillsForm(request.POST)
+        if form.is_valid():
+            new_bill = form.save(commit=False)
+            new_bill.building = building
+            new_bill.save()
+            return redirect('bills:building', building_key)
+        else:
+            print("form is not valid")
+    else:
+        form = BillsForm()
+    return render(request, 'buildings/building.html', {'building': building, 'form': form, 'bills': bills})
