cea6d36c859a9566cb2077f2f54355835da76e07
Berestianyi Ivan <berestvania@gmail.com>
2024-04-20 19:04:54 +0300

    added test chart arguments

--- buildings/views.py
+++ buildings/views.py
@@ -29,8 +29,53 @@ def building_page(request, building_key):
     building = Building.objects.get(pk=building_key)
     bills = Bill.objects.all().filter(building=building_key)
     total = Bill.total_sum(building_key=building_key)
+    user_data_2020 = [
+        {"label": "JAN", "y": 58200},
+        {"label": "FEB", "y": 59110},
+        {"label": "MAR", "y": 60320},
+        {"label": "APR", "y": 61440},
+        {"label": "MAY", "y": 62580},
+        {"label": "JUN", "y": 63190},
+        {"label": "JUL", "y": 64000},
+        {"label": "AUG", "y": 64290},
+        {"label": "SEP", "y": 65530},
+        {"label": "OCT", "y": 65300},
+        {"label": "NOV", "y": 65340},
+        {"label": "DEC", "y": 64530}
+    ]
+    user_data_2021 = [
+        {"label": "JAN", "y": 65100},
+        {"label": "FEB", "y": 66210},
+        {"label": "MAR", "y": 66540},
+        {"label": "APR", "y": 66680},
+        {"label": "MAY", "y": 67500},
+        {"label": "JUN", "y": 68850},
+        {"label": "JUL", "y": 69000},
+        {"label": "AUG", "y": 70130},
+        {"label": "SEP", "y": 71050},
+        {"label": "OCT", "y": 71500},
+        {"label": "NOV", "y": 72110},
+        {"label": "DEC", "y": 71820}
+    ]
+    user_data_2023 = [
+        {"label": "JAN", "y": 63450},
+        {"label": "FEB", "y": 63450},
+        {"label": "MAR", "y": 62664},
+        {"label": "APR", "y": 68650},
+        {"label": "MAY", "y": 53570},
+        {"label": "JUN", "y": 75750},
+        {"label": "JUL", "y": 69000},
+        {"label": "AUG", "y": 65630},
+        {"label": "SEP", "y": 71050},
+        {"label": "OCT", "y": 45700},
+        {"label": "NOV", "y": 75710},
+        {"label": "DEC", "y": 73530}
+    ]
     return render(request, 'buildings/building.html', {
         'building': building,
         'bills': bills,
         'total_sum': total,
+        "user_data_2021": user_data_2021,
+        "user_data_2020": user_data_2020,
+        "user_data_2023": user_data_2023
     })
