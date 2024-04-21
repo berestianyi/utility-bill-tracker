aa48d799625f6b569c293ceabb6de53a63def16a
Berestianyi Ivan <berestvania@gmail.com>
2024-04-22 01:19:53 +0300

    fixed form

--- bills/forms.py
+++ bills/forms.py
@@ -1,10 +1,7 @@
-from datetime import datetime
-
 from django import forms
-from bootstrap_datepicker_plus.widgets import DatePickerInput, MonthPickerInput
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
+from bootstrap_datepicker_plus.widgets import MonthPickerInput
 
-from bills.models import Building, BillSubCategory, Bill, BillCategory
+from bills.models import Bill, BillType
 
 
 class CustomBillForm(forms.CharField):
@@ -12,15 +9,7 @@ class CustomBillForm(forms.CharField):
 
 
 class BillsForm(forms.ModelForm):
-    category = forms.ModelChoiceField(queryset=BillCategory.objects.all(),
-                                      widget=forms.Select(attrs={
-                                          "hx-get": "/load_bill_category/",
-                                          "hx-target": "#id_name",
-                                          "class": 'form-select',
-                                          "id": "inputGroupSelect02",
-                                          "style": "background-color: #1b1d1e; color: #ececec;"
-                                      }))
-    name = forms.ModelChoiceField(queryset=BillSubCategory.objects.none(),
+    name = forms.ModelChoiceField(queryset=BillType.objects.all(),
                                   widget=forms.Select(attrs={
                                         "hx-get": "/load_tariff_and_measure_unit/",
                                         "hx-target": "#tariff_and_measure_unit",
@@ -48,25 +37,7 @@ class BillsForm(forms.ModelForm):
         }
     ))
 
-    created_at = forms.DateField(widget=DatePickerInput(
-        attrs={
-            "class": "form-control",
-            "placeholder": "Pay month",
-            "type": "text",
-            "style": "background-color: #1b1d1e; color: #ececec;",
-        },
-        options={
-            "format": "DD-MM-YYYY",
-        }
-    ))
-
     class Meta(object):
         model = Bill
-        fields = ['name', 'amount', 'month_paid', 'created_at']
+        fields = ['name', 'amount', 'month_paid']
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        if "category" in self.data:
-            category_id = int(self.data["category"])
-            self.fields['name'].queryset = BillSubCategory.objects.filter(bill_category=category_id)
