e84376df645080be84b768b4f7768a0804d3ebac
Berestianyi Ivan <berestvania@gmail.com>
2024-04-22 01:22:41 +0300

    added listView url

--- buildings/urls.py
+++ buildings/urls.py
@@ -6,5 +6,6 @@ app_name = 'buildings'
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_building/', views.add_building, name='add_building'),
-    path('building/<int:building_key>/', views.building_page, name='building'),
+    path('building/<slug:building_slug>/', views.BuildingListView.as_view(), name='building'),
+    path('building/<slug:building_slug>/<slug:bill_type_slug>/', views.BillTypeListView.as_view(), name='bill_type'),
 ]
