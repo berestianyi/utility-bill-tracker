ca4380d0c033c560d385d69cca799e5b4c1fbec0
Berestianyi Ivan <berestvania@gmail.com>
2024-04-22 01:23:04 +0300

    added slug to Building model

--- buildings/models.py
+++ buildings/models.py
@@ -1,18 +1,25 @@
 from django.db import models
+from django.utils.text import slugify
 from user.models import User
-from bills.models import BillSubCategory
+from bills.models import BillType
 
 
 class Building(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='buildings')
-    bill_sub_category = models.ManyToManyField(BillSubCategory, related_name='bill_sub_category', blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    bill_type = models.ManyToManyField(BillType, related_name='bill_type', blank=True)
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=50)
     home_mark = models.BooleanField(default=False)
     description = models.CharField(max_length=200, default='')
     currency = models.CharField(max_length=10, default='UAN')
+    slug = models.SlugField(unique=True, blank=True)
 
     objects = models.Manager()
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.address
