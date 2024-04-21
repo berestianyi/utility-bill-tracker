853962062ce7f34e75dee67f1b732cc95ce0e756
Berestianyi Ivan <berestvania@gmail.com>
2024-04-22 01:22:07 +0300

    rebuild views, added view classes

--- buildings/views.py
+++ buildings/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
 
 from bills.forms import BillsForm
-from bills.models import Bill
+from bills.models import Bill, BillType
 from buildings.forms import BuildingForm
 from buildings.models import Building
+from buildings.utils import BuildingDataMixin
 
 
 def index(request):
-    buildings = Building.objects.all()
-    return render(request, 'buildings/index.html', {'buildings': buildings})
+    form = BillsForm()
+
+    if request.user.is_authenticated:
+        buildings = Building.objects.filter(user=request.user)
+    else:
+        buildings = []
+    return render(request, 'buildings/index.html', {'buildings': buildings, 'form': form})
 
 
 def add_building(request):
@@ -25,57 +32,101 @@ def add_building(request):
     return render(request, 'buildings/add_building.html', {'form': form})
 
 
-def building_page(request, building_key):
-    building = Building.objects.get(pk=building_key)
-    bills = Bill.objects.all().filter(building=building_key)
-    total = Bill.total_sum(building_key=building_key)
-    user_data_2020 = [
-        {"label": "JAN", "y": 58200},
-        {"label": "FEB", "y": 59110},
-        {"label": "MAR", "y": 60320},
-        {"label": "APR", "y": 61440},
-        {"label": "MAY", "y": 62580},
-        {"label": "JUN", "y": 63190},
-        {"label": "JUL", "y": 64000},
-        {"label": "AUG", "y": 64290},
-        {"label": "SEP", "y": 65530},
-        {"label": "OCT", "y": 65300},
-        {"label": "NOV", "y": 65340},
-        {"label": "DEC", "y": 64530}
-    ]
-    user_data_2021 = [
-        {"label": "JAN", "y": 65100},
-        {"label": "FEB", "y": 66210},
-        {"label": "MAR", "y": 66540},
-        {"label": "APR", "y": 66680},
-        {"label": "MAY", "y": 67500},
-        {"label": "JUN", "y": 68850},
-        {"label": "JUL", "y": 69000},
-        {"label": "AUG", "y": 70130},
-        {"label": "SEP", "y": 71050},
-        {"label": "OCT", "y": 71500},
-        {"label": "NOV", "y": 72110},
-        {"label": "DEC", "y": 71820}
-    ]
-    user_data_2023 = [
-        {"label": "JAN", "y": 63450},
-        {"label": "FEB", "y": 63450},
-        {"label": "MAR", "y": 62664},
-        {"label": "APR", "y": 68650},
-        {"label": "MAY", "y": 53570},
-        {"label": "JUN", "y": 75750},
-        {"label": "JUL", "y": 69000},
-        {"label": "AUG", "y": 65630},
-        {"label": "SEP", "y": 71050},
-        {"label": "OCT", "y": 45700},
-        {"label": "NOV", "y": 75710},
-        {"label": "DEC", "y": 73530}
-    ]
-    return render(request, 'buildings/building.html', {
-        'building': building,
-        'bills': bills,
-        'total_sum': total,
-        "user_data_2021": user_data_2021,
-        "user_data_2020": user_data_2020,
-        "user_data_2023": user_data_2023
-    })
+class BuildingListView(BuildingDataMixin, ListView):
+    model = Bill
+    context_object_name = 'bills'
+
+    def get_queryset(self):
+        building_slug = self.kwargs['building_slug']
+        return Bill.objects.filter(building__slug=building_slug).order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        building_slug = self.kwargs['building_slug']
+        building = Building.objects.get(slug=building_slug)
+        context['building'] = building
+        context['bill_types'] = BillType.objects.all()
+        context['total_sum'] = Bill.total_sum(building_slug=building_slug)
+        context['form'] = BillsForm()
+        return context
+
+
+class BillTypeListView(BuildingDataMixin, ListView):
+    model = Bill
+    context_object_name = 'bills'
+
+    def get_queryset(self):
+        building_slug = self.kwargs['building_slug']
+        bill_type_slug = self.kwargs['building_slug']
+        bills = Bill.objects.filter(building__slug=building_slug)
+        return bills.filter(name__slug=bill_type_slug)
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        building_slug = self.kwargs['building_slug']
+        building = Building.objects.get(slug=building_slug)
+        context['building'] = building
+        context['bill_types'] = BillType.objects.all()
+        context['total_sum'] = Bill.total_sum(building_slug=building_slug)
+        context['form'] = BillsForm()
+        return context
+
+
+
+# def building_page(request, building_key):
+#     building = Building.objects.get(pk=building_key)
+#     bills = Bill.objects.all().filter(building=building_key)
+#     total = Bill.total_sum(building_key=building_key)
+#     form = BillsForm()
+#     user_data_2020 = [
+#         {"label": "JAN", "y": 58200},
+#         {"label": "FEB", "y": 59110},
+#         {"label": "MAR", "y": 60320},
+#         {"label": "APR", "y": 61440},
+#         {"label": "MAY", "y": 62580},
+#         {"label": "JUN", "y": 63190},
+#         {"label": "JUL", "y": 64000},
+#         {"label": "AUG", "y": 64290},
+#         {"label": "SEP", "y": 65530},
+#         {"label": "OCT", "y": 65300},
+#         {"label": "NOV", "y": 65340},
+#         {"label": "DEC", "y": 64530}
+#     ]
+#     user_data_2021 = [
+#         {"label": "JAN", "y": 65100},
+#         {"label": "FEB", "y": 66210},
+#         {"label": "MAR", "y": 66540},
+#         {"label": "APR", "y": 66680},
+#         {"label": "MAY", "y": 67500},
+#         {"label": "JUN", "y": 68850},
+#         {"label": "JUL", "y": 69000},
+#         {"label": "AUG", "y": 70130},
+#         {"label": "SEP", "y": 71050},
+#         {"label": "OCT", "y": 71500},
+#         {"label": "NOV", "y": 72110},
+#         {"label": "DEC", "y": 71820}
+#     ]
+#     user_data_2023 = [
+#         {"label": "JAN", "y": 63450},
+#         {"label": "FEB", "y": 63450},
+#         {"label": "MAR", "y": 62664},
+#         {"label": "APR", "y": 68650},
+#         {"label": "MAY", "y": 53570},
+#         {"label": "JUN", "y": 75750},
+#         {"label": "JUL", "y": 69000},
+#         {"label": "AUG", "y": 65630},
+#         {"label": "SEP", "y": 71050},
+#         {"label": "OCT", "y": 45700},
+#         {"label": "NOV", "y": 75710},
+#         {"label": "DEC", "y": 73530}
+#     ]
+#     return render(request, 'buildings/building.html', {
+#         'building': building,
+#         'bills': bills,
+#         'total_sum': total,
+#         'form': form,
+#         "user_data_2021": user_data_2021,
+#         "user_data_2020": user_data_2020,
+#         "user_data_2023": user_data_2023
+#     })
