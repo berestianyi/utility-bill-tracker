6ba348274857c129f26aff449e82997ac7cea9df
Berestianyi Ivan <berestvania@gmail.com>
2024-04-23 16:07:34 +0300

    created date_filter templatetag which returns previous date for list

---
+++ buildings/templatetags/date_filter.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def get_previous_date(bills, index):
+    if index == 0:
+        return None
+    return bills[index - 1].str_time
